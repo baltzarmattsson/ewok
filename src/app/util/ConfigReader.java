32c88a4b81907e5bde799ab4569398744a257748
Baltzar <baltzar@hotmail.se>
2016-11-23 14:30:27 +0100

    Jar file working as expected

--- src/app/util/ConfigReader.java
+++ src/app/util/ConfigReader.java
@@ -6,6 +6,9 @@ import app.Main;
 import app.model.Configuration;
 
 import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 
@@ -13,6 +16,9 @@ public class ConfigReader {
 
     private static Configuration configInstance;
     public static final String settingsPwd = "hej123";
+
+    private static final String fileName = "config.txt";
+    private static final String filePath = "config/";
 
 
 //    public static Configuration getTempConfig() {
@@ -42,11 +48,17 @@ public class ConfigReader {
 //    }
 
     public static void readConfigurationFile() {
+        File jarFile = null;
+        String finalPath = null;
         try {
 
             BufferedReader br;
-            br = new BufferedReader(new FileReader(new File(Main.class.getResource("config/config.txt").getFile())));
-//			br = new BufferedReader(new InputStreamReader(new FileInputStream(Main.class.getResource("/text/firstNames.txt").getFile()), StandardCharsets.UTF_8));
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(Main.class.getResource("config/config.txt").getFile()), StandardCharsets.UTF_8));
+
+            // Used from the jarfile
+//            jarFile = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+//            finalPath = jarFile.getParent() + File.separator + filePath + File.separator + fileName;
+//            br = new BufferedReader(new FileReader(new File(finalPath)));
 
             String s;
             HashMap<Integer, String> configAsText = new HashMap<Integer, String>();
@@ -60,12 +72,22 @@ public class ConfigReader {
         } catch (IOException e) {
             e.printStackTrace();
         }
+//        catch (URISyntaxException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public static void saveConfigurationFile(Configuration config) {
         BufferedWriter bw = null;
         try {
-            bw = new BufferedWriter(new FileWriter(new File(Main.class.getResource("config/config.txt").getFile())));
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(Main.class.getResource("config/config.txt").getFile()), StandardCharsets.UTF_8));
+
+            // Used from the jarfile
+//            File jarFile = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+//            String finalPath = jarFile.getParent() + File.separator + filePath + File.separator + fileName;
+//            bw = new BufferedWriter(new FileWriter(new File(finalPath)));
+
+
             // Kolla ifall config är null
             HashMap<Integer, String> configAsText = Util.parseConfigToString(config);
             for (int i = 0; i < configAsText.size(); i++) {
@@ -75,11 +97,15 @@ public class ConfigReader {
             bw.close();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
+        }
+//        catch (URISyntaxException e) {
+//            e.printStackTrace();
+//        }
+        finally {
             try {
-                bw.close();
+                if (bw != null)
+                    bw.close();
             } catch (IOException e) {
-                e.printStackTrace();
             }
         }
 
